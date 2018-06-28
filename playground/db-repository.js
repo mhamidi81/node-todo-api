5e6b3225bcb77ac1ae319c52757ed0d21689cc69
mhamidi81 <mohamed.hamidi@gmail.com>
2018-06-28 07:16:49 +0100

    add repository

---
+++ playground/db-repository.js
@@ -0,0 +1,26 @@
+/* db.collection('Todos').insertOne(
+        {
+            text: "Learn nodeJs",
+            completed: false
+        },
+        (err, result) => {
+            if(err) {
+                return console.log('Unable to insert todo \r\n', err);
+            }
+            console.log(JSON.stringify(result.ops, undefined, 2));
+        }
+    ); */
+
+    /* db.collection('Users').insertOne(
+        {
+            name: "Mohamed Hamidi",
+            age: 26,
+            location: "Casablanca"
+        },
+        (err, result) => {
+            if(err) {
+                return console.log('Unable to insert user \r\n', err);
+            }
+            console.log(JSON.stringify(result.ops, undefined, 2));
+        }
+    ); */
--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,4 +1,4 @@
-const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
 const config = require('../config/config');
 
 const dbUrl = `mongodb://${config.db.host}:${config.db.port}`;
@@ -11,31 +11,15 @@ MongoClient.connect(dbUrl, (err, client) => {
     }
     console.log('Connected to mongodb server');
     const db = client.db(dbName);
-    /* db.collection('Todos').insertOne(
+    db.collection('Todos').find(
         {
-            text: "Learn nodeJs",
-            completed: false
-        },
-        (err, result) => {
-            if(err) {
-                return console.log('Unable to insert todo \r\n', err);
-            }
-            console.log(JSON.stringify(result.ops, undefined, 2));
-        }
-    ); */
-
-    db.collection('Users').insertOne(
-        {
-            name: "Mohamed Hamidi",
-            age: 26,
-            location: "Casablanca"
-        },
-        (err, result) => {
-            if(err) {
-                return console.log('Unable to insert user \r\n', err);
-            }
-            console.log(JSON.stringify(result.ops, undefined, 2));
-        }
+          _id: new ObjectID('5b2f7844dfc88c22b4a8dcc9')
+        }).toArray().then(
+        docs => { 
+          docs.forEach(doc => console.log(doc.text, '=>', doc.completed) );
+        }, 
+        err => console.log(err)
+        
     );
 
     client.close();
