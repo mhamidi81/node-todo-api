d05c742ed4cddbd06cbd45ca57d738722b435737
mhamidi81 <mohamed.hamidi@gmail.com>
2018-07-08 10:47:36 +0100

    add patch todo ws

--- server/server.js
+++ server/server.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const _ = require('lodash');
 const {ObjectID} = require('mongodb');
-
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
-const { User } = require('./models/user');
 
 const port = process.env.PORT || 3000;
 
@@ -63,6 +62,34 @@ app.delete('/todos/:id', (req, res) => {
             return res.status(400).send({message: err.message});
         } 
     );
+});
+
+app.patch('/todos/:id', (req, res) => {
+    const id = req.params.id;
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send({message: `ID=${id} is not valid.`});
+    }
+    const body = _.pick(req.body, ['text', 'completed']);
+    if(_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+    
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true})
+        .then(
+            todo => {
+                if(!todo) {
+                    return res.status(404).send({message: `No todo found with id=${id}`});
+                } else {
+                    return res.status(200).send(todo);
+                }
+            },
+            err => {
+                return res.status(400).send({message: err.message});
+            }
+        );
 });
 
 app.listen(port, () => console.log(`Starting server on localhost:${port}`));
