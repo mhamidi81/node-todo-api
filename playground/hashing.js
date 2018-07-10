f7965023f8a31a331486d47009670e7d45852200
mhamidi81 <mohamed.hamidi@gmail.com>
2018-07-10 15:22:16 +0100

    hash user password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,28 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-const data = {
-    id: 10
-};
+const password = '123abc!';
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//     });
+// });
 
-const token = jwt.sign(data, 'somesecret');
-// jwt.io
-console.log(token);
-var decoded = jwt.verify(token, 'somesecret');
-console.log(decoded);
+const hashedPassword = '$2a$10$rFRdAnBXy1/vF4HazG9O3egy5j7rsHbm98a6OFSLqaMJAJzOBlA5e';
+bcrypt.compare(password, hashedPassword, (err, result) => {
+    console.log(result);
+});
+
+// const data = {
+//     id: 10
+// };
+
+// const token = jwt.sign(data, 'somesecret');
+// // jwt.io
+// console.log(token);
+// var decoded = jwt.verify(token, 'somesecret');
+// console.log(decoded);
 
 
 
